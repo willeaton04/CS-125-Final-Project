8972311b00987f82ce4c5cd819efc5bee85c9c18
willeaton04 <willeatonaz@gmail.com>
2025-11-24 10:12:05 -0800

    fixing bugs with schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,133 +1,127 @@
 # FAKE DATA
 
+DROP DATABASE IF EXISTS YouthGroup;
 CREATE DATABASE IF NOT EXISTS YouthGroup;
 USE YouthGroup;
 
 
-CREATE TABLE camp (
-    id INTEGER PRIMARY KEY,
-    FOREIGN KEY id REFERENCES event(id)
-
+CREATE TABLE Role (
+    id SMALLINT UNSIGNED PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100)
 );
 
-CREATE TABLE campRegistration (
-    id INTEGER PRIMARY KEY,
-    campId INTEGER,
-    studentId INT NOT NULL,
-    timestamp TIMESTAMP,
-    invoiceId INT NOT NULL UNIQUE,
-    FOREIGN KEY campId REFERENCES camp(id),
-    FOREIGN KEY invoiceid REFERENCES invoice(id),
-    FOREIGN KEY  studentid REFERENCES student(id)
-);
-
-CREATE TABLE invoice (
-    id INTEGER PRIMARY KEY,
-    parentId INTEGER,
-    timestamp TIMESTAMP,
-    amount INTEGER,
-    studentId INT,
-    FOREIGN KEY studentid REFERENCES student(id),
-    FOREIGN KEY parentid REFERENCES parent(id)
-);
-
-CREATE TABLE student (
-    id INTEGER PRIMARY KEY,
-    parentId INTEGER NOT NULL,
-    smallGroupId INTEGER,
-    Note VARCHAR(255),
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    email VARCHAR(50) UNIQUE,
-    phoneNumber VARCHAR(10) NOT NULL,
-    FOREIGN KEY parentid REFERENCES parent(id),
-    FOREIGN KEY smallGroupId REFERENCES smallGroup(id)
-
-);
-
-CREATE TABLE parent (
-    id INTEGER PRIMARY KEY,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    email VARCHAR(50),
-    phoneNumber VARCHAR(10) NOT NULL,
-    Note VARCHAR(255)
-);
-
-CREATE TABLE studentAttendance (
-    studentId INTEGER,
-    eventId INTEGER,
-    Timestamp TIMESTAMP,
-    FOREIGN KEY studentId references student(id),
-    FOREIGN KEY eventId references event(id),
-    PRIMARY KEY (studentId, eventId)
-
-);
-
-CREATE TABLE smallGroup (
-    id INTEGER PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL UNIQUE,
-    LeaderId Integer NOT NULL,
-    meetingTime TIMESTAMP,
-    FOREIGN KEY LeaderId REFERENCES Leader(id)
+CREATE TABLE Shift (
+    id SMALLINT UNSIGNED PRIMARY KEY,
+    start_time DATETIME,
+    end_time DATETIME
 );
 
 CREATE TABLE Leader (
-    id INTEGER PRIMARY KEY,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
+    id SMALLINT UNSIGNED PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50),
-    phoneNumber VARCHAR(10) NOT NULL,
-    Note VARCHAR(255),
-    dateJoined DATE,
-    Salary INTEGER,
-    foreign key roleId REFERENCES Role(id)
-
-
+    phone_number VARCHAR(10) NOT NULL,
+    note VARCHAR(255),
+    date_joined DATE,
+    salary SMALLINT UNSIGNED,
+    role_id SMALLINT UNSIGNED NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES Role(id)
 );
 
-CREATE TABLE LeaderRole (
-    LeaderId INTEGER,
-    RoleId INTEGER,
-    FOREIGN KEY LeaderId references Leader(id),
-    FOREIGN KEY RoleId references Role(id),
-    PRIMARY KEY (LeaderId, RoleId)
-
+CREATE TABLE SmallGroup (
+    id SMALLINT UNSIGNED PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    leader_id SMALLINT UNSIGNED NOT NULL,
+    meeting_time TIMESTAMP,
+    FOREIGN KEY (leader_id) REFERENCES Leader(id)
 );
 
-CREATE TABLE Role (
-    id INTEGER PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    Description VARCHAR(100)
+CREATE TABLE Parent (
+    id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50),
+    phone_number VARCHAR(10) NOT NULL,
+    note VARCHAR(255)
 );
 
-CREATE TABLE LeaderShift (
-    leaderId INTEGER,
-    shiftId INTEGER,
-    FOREIGN KEY leaderId REFERENCES Leader(id),
-    FOREIGN KEY shiftId REFERENCES shift(id),
-    PRIMARY KEY (leaderId, shiftId)
+CREATE TABLE Student (
+     id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+     parent_id SMALLINT UNSIGNED NOT NULL,
+     small_group_id SMALLINT UNSIGNED,
+     note VARCHAR(255),
+     first_name VARCHAR(50) NOT NULL,
+     last_name VARCHAR(50) NOT NULL,
+     email VARCHAR(50) UNIQUE,
+     phone_number VARCHAR(10) NOT NULL,
+     FOREIGN KEY (parent_id) REFERENCES Parent(id),
+     FOREIGN KEY (small_group_id) REFERENCES SmallGroup(id)
 );
 
-CREATE TABLE shift (
-    id INTEGER PRIMARY KEY,
-    startTime DATETIME,
-    endTime DATETIME
-
-);
-
-CREATE TABLE venue (
-    id INTEGER PRIMARY KEY,
+CREATE TABLE Venue (
+    id SMALLINT UNSIGNED PRIMARY KEY,
     address VARCHAR(100),
     description VARCHAR(150)
 );
 
-CREATE TABLE event (
-    id INTEGER PRIMARY KEY,
-    venueId INTEGER,
-    startTime DATETIME,
-    endTime DATETIME,
-    Description VARCHAR(256),
-    FOREIGN KEY venueId REFERENCES venue(id)
+CREATE TABLE Event (
+    id SMALLINT UNSIGNED PRIMARY KEY,
+    venue_id SMALLINT UNSIGNED,
+    start_time DATETIME,
+    end_time DATETIME,
+    description VARCHAR(256),
+    FOREIGN KEY (venue_id) REFERENCES Venue(id)
 );
 
+CREATE TABLE Camp (
+    id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (id) REFERENCES Event(id)
+);
+
+CREATE TABLE Invoice (
+    id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    parent_id SMALLINT UNSIGNED NOT NULL,
+    timestamp TIMESTAMP,
+    amount SMALLINT UNSIGNED,
+    student_id SMALLINT UNSIGNED NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES Student(id),
+    FOREIGN KEY (parent_id) REFERENCES Parent(id)
+);
+
+CREATE TABLE CampRegistration (
+    id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    camp_id SMALLINT UNSIGNED,
+    student_id SMALLINT UNSIGNED NOT NULL,
+    timestamp TIMESTAMP,
+    invoice_id SMALLINT NOT NULL UNIQUE,
+    FOREIGN KEY (camp_id) REFERENCES Camp(id),
+    FOREIGN KEY (invoice_id) REFERENCES Invoice(id),
+    FOREIGN KEY  (student_id) REFERENCES Student(id)
+);
+
+CREATE TABLE StudentAttendance (
+    student_id SMALLINT UNSIGNED,
+    event_id SMALLINT UNSIGNED,
+    timestamp TIMESTAMP,
+    FOREIGN KEY (student_id) references Student(id),
+    FOREIGN KEY (event_id) references Event(id),
+    PRIMARY KEY (student_id, event_id)
+);
+
+CREATE TABLE LeaderRole (
+    leader_id SMALLINT UNSIGNED,
+    role_id SMALLINT UNSIGNED,
+    FOREIGN KEY (leader_id) references Leader(id),
+    FOREIGN KEY (role_id) references Role(id),
+    PRIMARY KEY (leader_id, role_id)
+);
+
+CREATE TABLE LeaderShift (
+    leader_id SMALLINT UNSIGNED,
+    shift_id SMALLINT UNSIGNED,
+    FOREIGN KEY (leader_id) REFERENCES Leader(id),
+    FOREIGN KEY (shift_id) REFERENCES Shift(id),
+    PRIMARY KEY (leader_id, shift_id)
+);
