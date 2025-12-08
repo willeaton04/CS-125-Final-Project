d4f33efc701484d5ef70e3852ed9a58572ff5ba0
willeaton04 <willeatonaz@gmail.com>
2025-12-07 16:14:27 -0800

    worked on schema and graphql endpoints

--- db/schema.sql
+++ db/schema.sql
@@ -32,9 +32,9 @@ CREATE TABLE Leader (
 CREATE TABLE SmallGroup (
     id SMALLINT UNSIGNED PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
-    leader_id SMALLINT UNSIGNED NOT NULL,
+    leader_id SMALLINT UNSIGNED,
     meeting_time TIMESTAMP,
-    FOREIGN KEY (leader_id) REFERENCES Leader(id)
+    FOREIGN KEY (leader_id) REFERENCES Leader(id) ON DELETE SET NULL
 );
 
 CREATE TABLE Parent (
@@ -48,14 +48,14 @@ CREATE TABLE Parent (
 
 CREATE TABLE Student (
     id SMALLINT UNSIGNED PRIMARY KEY ,
-    parent_id SMALLINT UNSIGNED NOT NULL,
+    parent_id SMALLINT UNSIGNED,
     small_group_id SMALLINT UNSIGNED,
     note VARCHAR(255),
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE,
     phone_number VARCHAR(10) NOT NULL,
-    FOREIGN KEY (parent_id) REFERENCES Parent(id),
+    FOREIGN KEY (parent_id) REFERENCES Parent(id) ON DELETE SET NULL,
     FOREIGN KEY (small_group_id) REFERENCES SmallGroup(id)
 );
 
@@ -85,8 +85,8 @@ CREATE TABLE Invoice (
     timestamp TIMESTAMP,
     amount SMALLINT UNSIGNED,
     student_id SMALLINT UNSIGNED NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES Student(id),
-    FOREIGN KEY (parent_id) REFERENCES Parent(id)
+    FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE,
+    FOREIGN KEY (parent_id) REFERENCES Parent(id) ON DELETE CASCADE
 );
 
 CREATE TABLE CampRegistration (
@@ -96,23 +96,23 @@ CREATE TABLE CampRegistration (
     timestamp TIMESTAMP,
     invoice_id SMALLINT UNSIGNED NOT NULL UNIQUE,
     FOREIGN KEY (camp_id) REFERENCES Camp(id),
-    FOREIGN KEY (invoice_id) REFERENCES Invoice(id),
-    FOREIGN KEY  (student_id) REFERENCES Student(id)
+    FOREIGN KEY (invoice_id) REFERENCES Invoice(id) ON DELETE CASCADE ,
+    FOREIGN KEY  (student_id) REFERENCES Student(id) ON DELETE CASCADE
 );
 
 CREATE TABLE StudentAttendance (
     student_id SMALLINT UNSIGNED,
     event_id SMALLINT UNSIGNED,
     timestamp TIMESTAMP,
-    FOREIGN KEY (student_id) references Student(id),
-    FOREIGN KEY (event_id) references Event(id),
+    FOREIGN KEY (student_id) REFERENCES Student(id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES Event(id),
     PRIMARY KEY (student_id, event_id)
 );
 
 CREATE TABLE LeaderRole (
     leader_id SMALLINT UNSIGNED,
     role_id SMALLINT UNSIGNED,
-    FOREIGN KEY (leader_id) references Leader(id),
+    FOREIGN KEY (leader_id) REFERENCES Leader(id) ON DELETE CASCADE,
     FOREIGN KEY (role_id) references Role(id),
     PRIMARY KEY (leader_id, role_id)
 );
@@ -120,7 +120,7 @@ CREATE TABLE LeaderRole (
 CREATE TABLE LeaderShift (
     leader_id SMALLINT UNSIGNED,
     shift_id SMALLINT UNSIGNED,
-    FOREIGN KEY (leader_id) REFERENCES Leader(id),
+    FOREIGN KEY (leader_id) REFERENCES Leader(id) ON DELETE CASCADE,
     FOREIGN KEY (shift_id) REFERENCES Shift(id),
     PRIMARY KEY (leader_id, shift_id)
 );
